a28f8eb06be794e6b6fb9d77a103e99921313296
itsmartashub <coderawii@gmail.com>
2023-11-16 20:50:49 +0100

    feat(node): add Node.js script for more convenient (de)compressing `flex` files

---
+++ src/js/compress.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const AdmZip = require('adm-zip')
+
+const { formatCurrTime } = require('./utils')
+
+function compressToFlexFile(inputFolderPath, outputFlexFilePath) {
+	const zip = new AdmZip()
+
+	// Get the absolute path of the input folder
+	const absoluteInputPath = path.join(__dirname, inputFolderPath)
+
+	// Add the entire directory to the zip
+	zip.addLocalFolder(absoluteInputPath)
+
+	// Write the zip file
+	zip.writeZip(outputFlexFilePath)
+}
+
+let changesName = 'te'
+changesName = changesName.replaceAll(' ', '_')
+
+// const inputFolderPath = 'flex_decompressed'
+const inputFolderPath = '../flex_decompressed'
+const outputFlexFilePathDev = path.join(
+	__dirname,
+	'../flex_compressed/dev',
+	`gboardish-${formatCurrTime()}.flex`
+	// `gboardish--${changesName}.flex`
+)
+const outputFlexFilePath = path.join(__dirname, 'flex_compressed', 'gboardish.flex')
+
+/* DEV */
+compressToFlexFile(inputFolderPath, outputFlexFilePathDev)
+
+/* PRODUCTION */
+// compressToFlexFile(inputFolderPath, outputFlexFilePathDev)
+
+console.log({ inputFolderPath, outputFlexFilePath })
+console.log('Files compressed back to Flex successfully.')
---
+++ src/js/decompress.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const fs = require('fs')
+const AdmZip = require('adm-zip')
+
+function decompressFlexFile(inputFlexFilePath, outputFolderPath) {
+	try {
+		// Check if the Flex file exists before attempting to decompress
+		if (!fs.existsSync(inputFlexFilePath)) throw new Error('Flex file not found.')
+
+		const zip = new AdmZip(inputFlexFilePath)
+		zip.extractAllTo(outputFolderPath, true)
+		console.log('Flex file decompressed successfully.')
+	} catch (err) {
+		console.error('Error during decompression:', err.message)
+	}
+}
+
+// Example usage for decompression
+/* const inputFlexFilePath = path.join(
+	__dirname,
+	'flex_compressed',
+	'gboardish.flex'
+	)
+	
+const outputFolderPath = 'js/flex_decompressed'
+*/
+const inputFlexFilePath = path.join(__dirname, '../flex_compressed', 'gboardish.flex')
+
+const outputFolderPath = './src/flex_decompressed'
+decompressFlexFile(inputFlexFilePath, outputFolderPath)
+
+console.log({ inputFlexFilePath, outputFolderPath })
