6055c8a1af0e2c22cb32527ce8e0925e531ff545
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-09-29 22:38:39 +0200

    feat(emoji): Implement `emoji-key-popup` style

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'remove-candidate-word-var', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'add-emoji-key-popup-style', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
