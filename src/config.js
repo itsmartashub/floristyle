de2ef2c8640e0ceb6b86916f28b9ed972bc7f4e4
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-09-29 22:29:25 +0200

    refactor: Removed `candidate-word` var and replaced it with the body font size
    - This change ensures that emojis text are not cropped, providing a consistent visual experience across various text elements.
    
    Changes summary:
    - Adjusted font sizes to accommodate emojis, improving overall text presentation.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
-	VERSION: '3.1.0', // Version of the build
-	CHANGE_NAME: 'debugChanges', // Change name for debug builds, empty for production
+	VERSION: '4.0.0', // Version of the build
+	CHANGE_NAME: 'remove-candidate-word-var', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
