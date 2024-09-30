825552574d99ee7e86e5c89717adc2f2a9db75a7
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-09-30 22:36:09 +0200

    feat: Add styles to `clipboard-enable-history-button` to match the active theme

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'clipboard-item-change-shape-round-to-shape-round-hc', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'add-clipboard-enable-history-button-style', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
