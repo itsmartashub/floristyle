8bd003e8260796ad1c2f5378e1e0813d629f582b
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-09-30 17:38:46 +0200

    feat: Add capslock (`shiftstate=3`) style to all themes, not only to BL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'change-accent-hc-from-dark-primary-to-light-primaryContainer', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'add-capslocked-style-to-all-themes', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
