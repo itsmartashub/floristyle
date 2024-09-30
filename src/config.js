2edd966fb193fc4dd8525a905c3f4dd541a46b90
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-09-30 17:03:10 +0200

    fix: Update `--color-accent-hc` to `dynamic-light-color(primaryContainer)` for dark and oled themes
    - Addressed the issue with `--color-accent-hc` in dark/dark BL and oled/oled BL themes.
    - Replaced "dynamic-dark-color(primary)" with "dynamic-light-color(primaryContainer)".
    - This ensures accessibility by avoiding non-readable black colors on A14 devices with monochromatic monet color schemes (which "dynamic-dark-color(onPrimaryContainer)" causes)
    - Additionally, this update provides a visually pleasing accent color for the submit button when pressed.
    
    Changes summary:
    - Enhanced accessibility by updating accent colors, ensuring a readable and attractive interface, especially for A14 devices with specific color preferences.
    - Improved the visual feedback of the submit button when activated.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'fix-action-key-pressed-style', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'change-accent-hc-from-dark-primary-to-light-primaryContainer', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
