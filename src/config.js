52790aea409efeddc1286a8885d86a9a8f5d42e9
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-09-30 16:32:43 +0200

    fix: Improve `smartbar-action-key:pressed` style

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'add-emoji-key-popup-style', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'fix-action-key-pressed-style', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
