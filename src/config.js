7a3b50e8cca3c9c693937d7b49ce1f7ca824f651
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-09-30 22:19:54 +0200

    refactor: Increase the radius of the `clipboard-item` for BL themes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'overflow-customize-btn-pressed-add-bg', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'clipboard-item-change-shape-round-to-shape-round-hc', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
