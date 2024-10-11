24a789b61e29c6694be3990231169722f83e7d95
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-11 11:31:46 +0200

    feat(evening): Update keys-txt color from `inverseSurface` to `onSurface` to match the `Evening BL`
    - Changed the color of keys-txt to match the evening borderless theme (from `inverseSurface` to `onSurface`)
    - Improved visual consistency and readability in the evening theme
    
    Changes summary:
        - Updated the keys-txt color in the evening theme for better readability and consistency.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'void--candclip-and-suggestions-use-accenthc', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'evening--match-the-keys-txt-color-w-eveningBL', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
