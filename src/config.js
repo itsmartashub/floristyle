ae6b4c30448618a804f993dbfd67ac77fa48b7a5
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-11 12:36:50 +0200

    feat(midnight-abyss): Changed the keys-bg color to `surfaceContainerLow` to match the its BL pairs
    - Changed the keys-bg color from `surfaceContainer` to `surfaceContainerLow` to match the tiles bg in borderless themes
    - Improved visual consistency in the midnight and abyss themes
    
    Changes summary:
        - Updated the keys-bg color in the midnight and abyss themes for better visual consistency.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'void--revert-candidate-fg-to-accent-to-fix-monochromatic', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'midnight-&-abyss--keys-bg-from-surfaceContainer-to-surfaceContainerLow-to-match-its-BL', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
