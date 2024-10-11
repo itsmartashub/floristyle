9e47dbd1dfeb4d0eff6e385b99604557f074894a
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-11 13:35:07 +0200

    fix(bordered): Make numeric keys bg transparent on press to fix `Popup Visibility` style
    - Changed the background color of numeric keys to transparent when pressed in bordered themes
    - Resolved visual issue with key bg visible on edges behind popup when "Popup Visibility" setting is enabled
    
    Changes summary:
        - Fixed visual issue with numeric keys in bordered themes when "Popup Visibility" setting is enabled.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'midnight-&-abyss--keys-bg-from-surfaceContainer-to-surfaceContainerLow-to-match-its-BL', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'bordered--num-keys-bg-pressed-to-transparent', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
