16ed78ab99814476857b26c6eb6f9a025b8f3c31
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-18 17:55:33 +0200

    fix(key-popup): increase font size of key popups
    - Increased the font size of key popups to --fsz-heading
    - Increased the font size of focused key popups to --fsz-shift
    
    Changes summary:
        - Improved the readability of key popups by increasing their font size.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'evenings--board bg from surfaceContainerLow to surfaceContainer', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'all--increase key popups font size', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
