7f864d2a7d3d3cf20d26364e6072fe265064be70
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-18 19:17:20 +0200

    feat(voids-blackouts): update theme colors to be even more subdued
    - Updated `--color-keys-bg`, `--color-tiles-bg`, `--color-accent-dark` (bordered), and `--color-incognito` colors in Void and Blackout themes
    - Ensured a darker and more immersive visual experience for these OLED themes
    
    Changes summary:
        - Darkened the color scheme of the Void and Blackout themes for a more subdued look.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'nightfall BL -- fix border-radius not being circle for candidate-clip', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'void and blackout -- update keys, tiles, and incognito colors to darker', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
