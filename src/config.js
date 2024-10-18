f64ec901f87d796683d3ff730f622755f41d362b
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-18 16:19:58 +0200

    fix(evenings): switch the bg of keys and incognito to match the Gboard
    - --color-keys-bg/--color-tiles-bg from `surfaceContainerHigh` to `surfaceContainerHighest`
    - --color-incognito from `surfaceContainerHighest` to `surfaceContainerHigh`

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
-	VERSION: '4.0.0', // Version of the build
-	CHANGE_NAME: 'bordered--num-keys-bg-pressed-to-transparent', // Change name for debug builds, empty for production
+	VERSION: '4.0.1', // Version of the build
+	CHANGE_NAME: 'evenings--switch keys-bg and incognito bg', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
