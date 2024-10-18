429cc35bea1e90e1b967e918bfb77d5dae230a3d
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-18 18:14:31 +0200

    feat(dark/oled): update smartbar toggle buttons to match latest gboard themes
    - Updated dark and OLED themes to use `--accent-bg` and `--color-board-bg` for foreground of smartbar shared and extended toggle buttons
    - Ensured consistency with the latest Gboard themes for a cohesive look and feel
    
    Changes summary:
        - Updated dark and OLED themes to match the latest Gboard themes for smartbar toggle buttons.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'all dark bordered--update popup bg to incognito bg', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'all darks and oleds--make shared and extended toggle button bg accent and fg like board-bg', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
