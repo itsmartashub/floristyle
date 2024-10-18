e88cbdbe87ed119275d6dfa00c09bd3a0b18059c
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-18 18:21:22 +0200

    fix(abyss BL): `border-radius` for clipboard item not being `rounded-hc`
    - Fixed border-radius for clipboard item in Abyss BL theme to be rounded-hc, consistent with other borderless themes
    - Ensured visual consistency across all borderless themes
    
    Changes summary:
        - Fixed the border-radius issue for the clipboard item in the Abyss BL theme.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'all darks and oleds--make shared and extended toggle button bg accent and fg like board-bg', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'abyss BL -- fix border-radius not being rounded-hc for clipboard item', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
