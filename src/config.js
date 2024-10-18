0365bd9dfea8fecdec89a385b791879156d36988
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-18 18:25:27 +0200

    fix(nightfall BL): border-radius for `candidate-clip` not being `--shape-circle`
    - Fixed border-radius for `candidate-clip` in Nightfall BL theme to be `--shape-circle`, consistent with other borderless themes
    - Ensured visual consistency across all borderless themes
    
    Changes summary:
        - Fixed the border-radius issue for the `candidate-clip` in the Nightfall BL theme.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'abyss BL -- fix border-radius not being rounded-hc for clipboard item', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'nightfall BL -- fix border-radius not being circle for candidate-clip', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
