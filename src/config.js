c10a615b1fec014930a11e0af3151d3a616c9ad1
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-18 18:08:37 +0200

    feat(dark/oled): update `key-popup` background color
    - Updated key-popup background color to use `--color-incognito` in all dark/oled bordered themes
    - Improved visual appeal by differentiating `key-popup` background from `keys-bg`
    
    Changes summary:
        - Enhanced the visual distinction between `key-popup` and `keys-bg` in dark/oled bordered themes.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'all--increase key popups font size', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'all dark bordered--update popup bg to incognito bg', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
