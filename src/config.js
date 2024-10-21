ee2858ea6b359f1e5573e74597c2f1a0cebaabab
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-21 21:51:36 +0200

    feat(sunshine): add subtle border to key popup
    - Added a subtle border (`--color-board-bg`) to the key popup in the sunshine theme
    - Improved the visual separation between the key popup and the background, enhancing user experience
    
    Changes summary:
        - Enhanced the sunshine theme by adding a subtle border to the key popup for better visual separation.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'abyss and sunshine -- subdued keys bg ', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'sunshine -- add subtle border to key popup', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
