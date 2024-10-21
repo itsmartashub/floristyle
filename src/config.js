7d056178bbfc5b2088950b5dbcee1aef5e2aaac9
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2024-10-21 21:50:45 +0200

    fix(abyss-midnight): subdued keys background color
    - Subdued the keys background color in the abyss and midnight themes to reduce visual distraction
    - Improved the overall aesthetic of the OLED themes by reducing the contrast between keys and background
    
    Changes summary:
        - Toned down the keys background color in the abyss and midnight themes for a more pleasant OLED experience.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
 	VERSION: '4.0.1', // Version of the build
-	CHANGE_NAME: 'abyss and midnight -- subdued incognito color', // Change name for debug builds, empty for production
+	CHANGE_NAME: 'abyss and sunshine -- subdued keys bg ', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
