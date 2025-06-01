cd10896dadd50d0e0d4432dd304eab6d949f3d88
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-06-01 17:48:21 +0200

    chore(version): Bump to `5.0.1`

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
-	VERSION: '5.0.1-debug', // Version of the build
-	CHANGE_NAME: 'Rewrite action tiles to match Gboard style with new SnyggV2 props', // Change name for debug builds, empty for production
+	VERSION: '5.0.1', // Version of the build
+	CHANGE_NAME: '', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
