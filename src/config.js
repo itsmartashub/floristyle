02f6722864745c4cac838fb6745981c450afbe7f
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-06-10 19:43:10 +0200

    refactor(clipboard): improve clipboard history styling and refactor element names

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration for builds
 const config = {
-	VERSION: '5.0.1', // Version of the build
-	CHANGE_NAME: '', // Change name for debug builds, empty for production
+	VERSION: '5.1.0-debug', // Version of the build
+	CHANGE_NAME: 'improve clipboard history styling and refactor element names', // Change name for debug builds, empty for production
 	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
 }
 
