92572dce1d060360714f4534e9b2dddcabab691b
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-06-10 19:49:42 +0200

    chore(version): bump to `5.1.0`

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 // Configuration for builds
 const config = {
-	VERSION: '5.1.0-debug', // Version of the build
-	CHANGE_NAME: 'improve clipboard history styling and refactor element names', // Change name for debug builds, empty for production
-	// BUILD_FOLDER: 'debug', // Default build folder, can be 'debug' or 'prod'
+	/* The build version */
+	VERSION: '5.1.0',
+
+	/* Change name for debug builds, empty for production */
+	CHANGE_NAME: '', 
 }
 
 module.exports = config
